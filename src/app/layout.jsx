4f9c40e5082ae1e859fb96a24e3bd87a794f27a5
Mehmet Can Erturk <cannhann42@gmail.com>
2023-09-27 14:13:33 +0300

    Fundamental changes made.

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Next.js E-commerce website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  )
+}

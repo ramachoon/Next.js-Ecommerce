16e7961c0a23e8d2f5392d6d76e3ef2fc48696b9
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-08 21:42:35 +0300

    Modal slice created to manage dialogs.

---
+++ src/lib/redux/modal.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+// Initial state
+const initialState = {
+  loginModal: false,
+}
+
+// Slice
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    toggleModal: (state, action) => {
+      state[action.payload] = !state[action.payload]
+    },
+  },
+})
+
+
+export const { toggleModal } = modalSlice.actions
+export default modalSlice.reducer

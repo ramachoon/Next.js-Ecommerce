e1457f83a1fdd38af45ed5c9befd55ebc84d1897
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-13 22:04:31 +0300

    Add the SessionProvider to the currently Provider

--- src/lib/Provider.jsx
+++ src/lib/Provider.jsx
@@ -2,11 +2,14 @@
 
 import { Provider as ReduxProvider } from "react-redux"
 import store from "@/lib/redux"
+import { SessionProvider } from "next-auth/react"
 
 export default function Provider({ children }) {
   return (
     <ReduxProvider store={store}>
-      {children}
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </ReduxProvider>
   )
 }

5018ddf029867e971229181347c0b1091d218e7f
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-16 13:54:02 +0300

    Create a helper to use regex constants
    - Create the helper
    - Add email regex

---
+++ src/lib/helpers/regex.jsx
@@ -0,0 +1,3 @@
+// Email regex from https://emailregex.com/
+// Use this regex to validate an email address
+export const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/

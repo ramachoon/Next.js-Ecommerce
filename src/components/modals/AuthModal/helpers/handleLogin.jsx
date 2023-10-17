9c194cabc054ce11bf6b4ef863123e4f29cc587b
Mehmet Can Erturk <cannhann42@gmail.com>
2023-10-17 13:11:43 +0300

    Create API connection for login and registration
    - Update form submit listeners to connect to the authentication API

--- src/components/modals/AuthModal/helpers/handleLogin.jsx
+++ src/components/modals/AuthModal/helpers/handleLogin.jsx
@@ -1,3 +1,30 @@
+import { signIn } from "next-auth/react"
+import axios from "axios"
+
 export default async function handleLogin(email, password) {
-  console.log(email, password)
+  try {
+    // Try to login
+    const res = await axios.post("/api/user/login", {
+      email,
+      password,
+    })
+
+    // If the login was successful
+    if (res.data.status === 200) {
+      signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+      })
+    }
+
+    // Data coming from the server
+    // Temporary, will be removed in the future
+    console.log(res.data)
+
+    // TODO: Show messages to the user
+
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/components/modals/AuthModal/helpers/handleRegister.jsx
+++ src/components/modals/AuthModal/helpers/handleRegister.jsx
@@ -1,3 +1,21 @@
+import axios from "axios"
+
 export default async function handleRegister(email, password) {
-  console.log(email, password)
+  try {
+    // Try to register
+    const res = await axios.post("/api/user/register", {
+      email,
+      password
+    })
+
+    // Data coming from the server
+    // Temporary, will be removed in the future
+    console.log(res.data)
+
+    // TODO: Login automatically after registration is successful
+    // TODO: Show messages to the user
+
+  } catch (error) {
+    console.log(error)
+  }
 }
